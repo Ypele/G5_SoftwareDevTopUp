bf46d9a82861619a843e50078f6ed9e8c3f6cd4a
Alexand3r <andrei.tudor27@yahoo.com>
2017-05-12 11:07:58 +0200

    Styled Components

--- src/Booking/BookingList.js
+++ src/Booking/BookingList.js
@@ -1,51 +1,56 @@
 import React, { Component } from 'react';
 // import BookingItem from './BookingItem'
 import { Icon, Table } from 'semantic-ui-react'
+import * as s from './styles'
+
 // const initialState ={
 //     tableRows:[{bookID:0,name:"0test"},{bookID:1,name:"1test"}]
 // }
+
+
 class BookingList extends Component {
     render() {
         return (
-                <Table celled>
-      <Table.Header>
-        <Table.Row>
-          <Table.HeaderCell>Name</Table.HeaderCell>
-          <Table.HeaderCell>Status</Table.HeaderCell>
-          <Table.HeaderCell>Notes</Table.HeaderCell>
-        </Table.Row>
-      </Table.Header>
+            <s.BookingDiv>
+                <s.BookingTable celled>
+                    <s.TableHeader>
+                        <Table.Row>
+                            <s.TableHeaderCell>Name</s.TableHeaderCell>
+                            <s.TableHeaderCell>Status</s.TableHeaderCell>
+                            <s.TableHeaderCell>Notes</s.TableHeaderCell>
+                        </Table.Row>
+                    </s.TableHeader>
 
-      <Table.Body>
-        <Table.Row>
-          <Table.Cell>No Name Specified</Table.Cell>
-          <Table.Cell>Unknown</Table.Cell>
-          <Table.Cell negative>None</Table.Cell>
-        </Table.Row>
-        <Table.Row positive>
-          <Table.Cell>Jimmy</Table.Cell>
-          <Table.Cell>
-            <Icon name='checkmark' />
-            Approved
-          </Table.Cell>
-          <Table.Cell>None</Table.Cell>
-        </Table.Row>
-        <Table.Row>
-          <Table.Cell>Jamie</Table.Cell>
-          <Table.Cell>Unknown</Table.Cell>
-          <Table.Cell positive>
-            <Icon name='close' />
-            Requires call
-          </Table.Cell>
-        </Table.Row>
-        <Table.Row negative>
-          <Table.Cell>Jill</Table.Cell>
-          <Table.Cell>Unknown</Table.Cell>
-          <Table.Cell>None</Table.Cell>
-        </Table.Row>
-      </Table.Body>
-    </Table>
-
+                    <Table.Body>
+                        <Table.Row>
+                            <s.TableData>No Name Specified</s.TableData>
+                            <s.TableData>Unknown</s.TableData>
+                            <s.TableData negative>None</s.TableData>
+                        </Table.Row>
+                        <Table.Row positive>
+                            <s.TableData>Jimmy</s.TableData>
+                            <s.TableData>
+                                <Icon name='checkmark' />
+                                Approved
+                            </s.TableData>
+                            <s.TableData>None</s.TableData>
+                        </Table.Row>
+                        <Table.Row>
+                            <s.TableData>Jamie</s.TableData>
+                            <s.TableData>Unknown</s.TableData>
+                            <s.TableData positive>
+                                <Icon name='close' />
+                                Requires call
+                            </s.TableData>
+                        </Table.Row>
+                        <Table.Row negative>
+                            <s.TableData>Jill</s.TableData>
+                            <s.TableData>Unknown</s.TableData>
+                            <s.TableData>None</s.TableData>
+                        </Table.Row>
+                    </Table.Body>
+                </s.BookingTable>
+            </s.BookingDiv>
         );
     }
 }
---
+++ src/Booking/styles.js
@@ -0,0 +1,44 @@
+import styled from 'styled-components'
+import { Icon, Table } from 'semantic-ui-react'
+
+export const BookingDiv = styled.div`
+margin:50px;
+border:3px royalblue;
+border-radius:5px;
+background:royalblue;
+color:white;
+`
+export const BookingTable = styled(Table) `
+border:3px royalblue;
+border-radius:5px;
+background:#efefef;
+color:black;
+width: 100%;
+border-collapse: collapse;
+`
+
+export const TableHeader = styled(Table.Header) `
+    padding: 15px;
+    text-align: left;
+    border: 1px solid white;
+`
+
+export const TableData = styled(Table.Cell) `
+    padding: 15px;
+    text-align: left;
+    border: 1px solid white;
+    &:hover {
+   background-color: green;
+   color:white;
+}
+`
+
+export const TableHeaderCell = styled(Table.HeaderCell) `
+    padding: 15px;
+    text-align: left;
+    border: 1px solid white;
+   &:hover {
+   background-color: royalblue;
+   color:white;
+}
+`
